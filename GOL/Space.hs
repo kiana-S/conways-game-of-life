7412b9e3a742ade117ff0a505bafb92576d12e8e
kiana-S <kiana.a.sheibani@gmail.com>
2021-12-20 15:25:54 -0500

    Fix GOL.Space

--- GOL/Space.hs
+++ GOL/Space.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module GOL.Space where
 
 import Data.Functor.Rep
