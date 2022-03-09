c42111017279b2bc09ab0ef40b331b80929a1ec3
kiana-S <kiana.a.sheibani@gmail.com>
2022-03-09 13:49:35 -0500

    Remove unnecessary flake-compat input

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
